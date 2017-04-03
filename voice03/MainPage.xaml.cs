36dbae3e900915766c5099505bacf0c7e911d961
Pedro Orgeira <poc.desarrollo@hotmail.com>
2017-04-03 19:34:53 +0200

    Con errores, hasta el comienzo del reconocimiento

--- voice03/MainPage.xaml.cs
+++ voice03/MainPage.xaml.cs
@@ -30,6 +30,7 @@ namespace voice03
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        //mis objetillos
         private SpeechRecognizer speechRecognizer;
         private IAsyncOperation<SpeechRecognitionResult> recognitionOperation;
         private CoreDispatcher dispatcher;
@@ -42,15 +43,17 @@ namespace voice03
             this.InitializeComponent();
         }
 
-        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        protected async override void OnNavigatedTo(NavigationEventArgs e) //cuando llegas
         {
             dispatcher = CoreWindow.GetForCurrentThread().Dispatcher;
 
+            //comprobación de si tengo permiso sobre el micrófono; si tengo, inicio el proceso (InitializeRecognizer)
             bool tengoPermiso = await AudioCapturePermissions.RequestMicrophonePermission();
             if (tengoPermiso)
             {
                 Language speechLanguage = SpeechRecognizer.SystemSpeechLanguage;
-                await InitializeRecognizer(SpeechRecognizer.SystemSpeechLanguage);
+                //await InitializeRecognizer(SpeechRecognizer.SystemSpeechLanguage);
+                await InitializeRecognizer(speechLanguage); ;
             } else
             {
                 resultadosTB.Visibility = Visibility.Visible;
@@ -59,18 +62,58 @@ namespace voice03
         }
 
         private async Task InitializeRecognizer(Language recognizerLanguage)
-        {
+        {   //inicialiación del reconocedor
             if (speechRecognizer != null)
             {
                 speechRecognizer.StateChanged -= SpeechRecognizer_StateChanged;
+
+                this.speechRecognizer.Dispose();
+                this.speechRecognizer = null;
             }
+
+            try
+            {
+                //cargar el fichero de la grmática
+                string fileName = String.Format("SRGS\\SRGSColors.xml");
+                StorageFile grammaContentFile = await Package.Current.InstalledLocation.GetFileAsync(fileName);
+
+                //inicializamos el objeto reconocedor           
+                speechRecognizer = new SpeechRecognizer(recognizerLanguage);
+
+                //activa el feedback al usuario
+                speechRecognizer.StateChanged += SpeechRecognizer_StateChanged;
+
+                //compilamos la gramática
+                SpeechRecognitionGrammarFileConstraint grammarConstraint = new SpeechRecognitionGrammarFileConstraint(grammaContentFile);
+                speechRecognizer.Constraints.Add(grammarConstraint);
+                SpeechRecognitionCompilationResult compilationResult = await speechRecognizer.CompileConstraintsAsync();
+
+                //si no hubo éxito...
+                if (compilationResult.Status != SpeechRecognitionResultStatus.Success)
+                {
+                    resultadosTB.Visibility = Visibility.Visible;
+                    resultadosTB.Text = "Error compilando la gramática";
+                }
+                else
+                {
+                    //si hubo éxito 
+                    resultadosTB.Visibility = Visibility.Visible;
+                    resultadosTB.Text = "Gramática compilada, a por ello";
+                }
+            }
+            catch (Exception e)
+            {
+                var messageDialog = new Windows.UI.Popups.MessageDialog(e.Message, "Excepción chunga: ");
+                await messageDialog.ShowAsync();
+            }            
+
         }
 
         private async void SpeechRecognizer_StateChanged(SpeechRecognizer sender, SpeechRecognizerStateChangedEventArgs args)
-        {
+        {//feedback al usuario del estado del reconocimiento de texto
             await dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
             {
-            resultadosTB.Text = "Speech recognizer state: " + args.State.ToString();
+            resultadosTB.Text = "Estado de reconocimiento de texto: " + args.State.ToString();
             });
         }
 
